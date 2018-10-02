c4d17c10c2ffb9c5d5a2481bef02d036864bbf65
Sages11 <power_sarah@yahoo.com>
2018-10-02 15:30:08 -0800

    Made functions more robust. Working on getting all years in a data frame.

--- code/age_length.R
+++ code/age_length.R
@@ -35,8 +35,76 @@ age_df <- df1%>%
 
 
 # analysis ----
-df <- data_prep(age_df, 2003)
+year_vector <- sort(unique(age_df$yyear))
+# remove years 1986-88 since data is too sparse to analyse.
+year_vector <- year_vector[26:length(year_vector)]
 
+# create a dataframe with information for each year
+
+variable_names <-c("year", 
+                   "pi1", "pi.se1", "mu1", "mu.se1", "sigma1", "sigma.se1", 
+                   "pi2", "pi.se2", "mu2", "mu.se2", "sigma2", "sigma.se2", 
+                   "pi3", "pi.se3", "mu3", "mu.se3", "sigma3", "sigma.se3", 
+                   "pi4", "pi.se4", "mu4", "mu.se4", "sigma4", "sigma.se4", 
+                   "chi_pvalue", "chi_df")
+variables <- length(variable_names)
+iterations <- length(year_vector) # 
+
+data_all_years <- matrix(ncol = variables, nrow = iterations)
+
+for(i in year_vector){
+  i <- 1
+  df <- data_prep(age_df, year_vector[i])
+  (fitpro <- mix(as.mixdata(df), mixparam(mu=c(30,50,65,75), sigma=c(5,5,5,5)), dist='gamma', iterlim=5000)) 
+  plot(fitpro, main=year_vector[i])
+  data_all_years[i,] <-c(year_vector[i], 
+                fitpro$parameters$pi[1], fitpro$se$pi.se[1], 
+                fitpro$parameters$mu[1], fitpro$se$mu.se[1],
+                fitpro$parameters$sigma[1], fitpro$sigma$mu.se[1],
+                fitpro$parameters$pi[2], fitpro$se$pi.se[2], 
+                fitpro$parameters$mu[2], fitpro$se$mu.se[2],
+                fitpro$parameters$sigma[2], fitpro$sigma$mu.se[2],
+                fitpro$parameters$pi[3], fitpro$se$pi.se[3], 
+                fitpro$parameters$mu[3], fitpro$se$mu.se[3],
+                fitpro$parameters$sigma[3], fitpro$sigma$mu.se[3],
+                fitpro$parameters$pi[4], fitpro$se$pi.se[4], 
+                fitpro$parameters$mu[4], fitpro$se$mu.se[4],
+                fitpro$parameters$sigma[4], fitpro$sigma$mu.se[4],
+                fitpro$P, fitpro$df)
+}
+summary(fitpro)
+data_all_years
+data.frame(data_all_years)
+class(data_all_years)
+
+df <- data_prep(age_df, 2015)
 (fitpro <- mix(as.mixdata(df), mixparam(mu=c(30,50,65,75), sigma=c(5,5,5,5)), dist='gamma', iterlim=5000)) 
 plot(fitpro)
+summary(fitpro)
+fitpro$parameters$pi[1]
 
+iterations = 10
+variables = 2
+
+output <- matrix(ncol=variables, nrow=iterations)
+
+for(i in 1:iterations){
+  output[i,] <- runif(2)
+  
+}
+
+output
+output[i,] <-c(i, 
+               1, fitpro$parameters$pi[1], fitpro$se$pi.se[1], 
+               fitpro$parameters$mu[1], fitpro$se$mu.se[1],
+               fitpro$parameters$sigma[1], fitpro$sigma$mu.se[1],
+               2, fitpro$parameters$pi[2], fitpro$se$pi.se[2], 
+               fitpro$parameters$mu[2], fitpro$se$mu.se[2],
+               fitpro$parameters$sigma[2], fitpro$sigma$mu.se[2],
+               3, fitpro$parameters$pi[3], fitpro$se$pi.se[3], 
+               fitpro$parameters$mu[3], fitpro$se$mu.se[3],
+               fitpro$parameters$sigma[3], fitpro$sigma$mu.se[3],
+               4, fitpro$parameters$pi[4], fitpro$se$pi.se[4], 
+               fitpro$parameters$mu[4], fitpro$se$mu.se[4],
+               fitpro$parameters$sigma[4], fitpro$sigma$mu.se[4],
+               fitpro$p, fitpro$df)
